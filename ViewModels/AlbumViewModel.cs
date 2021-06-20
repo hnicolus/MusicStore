50b45cbb56df60ce4ada7cc87cede9d59f6d5fb6
hnicolus <nicolusmaluleke@gmail.com>
2021-06-20 14:37:05 +0200

    Made AlbumViewModel inherit from BaseViewModel to make it observable. Added image load support  and Decoded bit image to costum width.

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -1,7 +1,37 @@
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using MusicStore.Models;
+using ReactiveUI;
+
 namespace MusicStore.ViewModels
 {
-    public class AlbumViewModel
+    public class AlbumViewModel  :ViewModelBase
     {
+        private readonly Album _album;
+
+        public AlbumViewModel(Album album)
+        {
+            _album = album;
+        }
+
+        public string Artist => _album.Artist;
+
+        public string Title => _album.Title;
+        private Bitmap? _cover;
+
+        public Bitmap? Cover
+        {
+            get => _cover;
+            private set => this.RaiseAndSetIfChanged(ref _cover, value);
+        }
+        public async Task LoadCover()
+        {
+            await using (var imageStream = await _album.LoadCoverBitmapAsync())
+            {
+                Cover = await Task.Run(() => Bitmap.DecodeToWidth(imageStream, 400));
+            }
+        }
+        
         
     }
 }
