5f8448efd7ce97f967548dfbc171e2a02bc0dd2e
hnicolus <nicolusmaluleke@gmail.com>
2021-06-20 14:34:33 +0200

    Added event Subscribe to dialog closing.

--- Views/MusicStoreWindow.axaml.cs
+++ Views/MusicStoreWindow.axaml.cs
@@ -1,17 +1,25 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using MusicStore.ViewModels;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System;
+
 
 namespace MusicStore.Views
 {
-    public class MusicStoreWindow : Window
+    public class MusicStoreWindow :  ReactiveWindow<MusicStoreViewModel>
     {
         public MusicStoreWindow()
         {
             InitializeComponent();
+            this.WhenActivated(d => d(ViewModel.BuyMusicCommand.Subscribe(Close)));
 #if DEBUG
             this.AttachDevTools();
 #endif
+            
         }
 
         private void InitializeComponent()
